e87255943a94a1a5e693b97e1f8d410942a65fec
Jinhong Choi <jinhong.choi@gmail.com>
2020-06-11 15:46:32 -0700

    instance gen

---
+++ jobs/gen_inst_no_order.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import json
+import random
+import six
+
+data = {}
+data['name'] = "test1"
+data['jobs'] = {}
+
+print("Generating instance
+print("Enter the number of jobs", end='')

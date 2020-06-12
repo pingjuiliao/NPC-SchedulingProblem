9d8bd6d0e202b87be2b826dd0eb2ae71d8eaf5f3
Jinhong Choi <jinhong.choi@gmail.com>
2020-06-11 17:04:10 -0700

    fixed instance generator

--- jobs/inst_gen_order.py
+++ jobs/inst_gen_order.py
@@ -3,6 +3,7 @@
 import json
 import random
 import six
+import pprint
 
 data = {}
 data['name'] = "test1"
@@ -20,27 +21,39 @@ l_load = int(six.moves.input())
 # Set maximum order
 print("Enter maximum dependency: ", end="")
 m_order = int(six.moves.input())
+if m_order >= n_job:
+    m_order = n_job-1
+
 
 # Total load
 tot_load = 0;
 
 for i in range(1, n_job+1):
-    this_load = random.randint(1, l_load+1)
+    # choose load for this job
+    this_load = random.randint(1, l_load)
     tot_load += this_load
-    this_order = random.randint(0, m_order)
-    if this_order > (i-1):
-        this_order = i-1
 
-    order_list = random.sample(range(1, this_order+1), this_order)
+    # choose number of dependencies for this job
+    this_order = random.randint(1, m_order)
+    # if this_order > (i-1):
+    #     this_order = i-1
+
+    # pick dependencies
     dependency = []
-    for e in order_list:
-        dependency.append("job"+str(e))
+    if random.randint(0,1) == 1:
+        order_list = random.sample(range(1, n_job+1), this_order)
+        for e in order_list:
+            dependency.append("job"+str(e))
+
+        # If dependency includes current job, removes it
+        if ('job'+str(i)) in dependency:
+            dependency.remove('job'+str(i))
 
     data['jobs']['job'+str(i)] = [this_load, dependency]
 
 json_obj = json.dumps(data, indent = 3)
 print("Total load %d" %(tot_load))
-print(data)
+pprint.pprint(data)
 
 with open('case_order.json', 'w') as outfile:
     outfile.write(json_obj)
